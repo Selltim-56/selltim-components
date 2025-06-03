bccc1186749237ee793e0c76ca35d580adbbe415
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2025-06-03 13:14:34 +0200

    preserve directives

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
 import dtsPlugin from "vite-plugin-dts";
+import preserveDirectives from "rollup-preserve-directives";
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    dtsPlugin()
+    dtsPlugin(),
+    preserveDirectives()
   ],
   build: {
     lib: {
